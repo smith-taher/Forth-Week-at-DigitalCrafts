09dd69840df2a86c16ec3f820cab0dbdb723ba65
Joel Smith <joelsmith@Joels-MacBook-Pro.local>
2018-02-27 14:12:04 -0500

    updated PracticeGallery2

--- AlgorithmFriday.js
+++ AlgorithmFriday.js
@@ -13,13 +13,38 @@
 //   }
 //   steamrollArray([1, [2], [3, [[4]]]]);
   
-function steamrollArray(arr) {
-  var str = arr.join('');
-  str = str.slice(4, 1);
-  console.log(str);
-  var newarr;
+// // function steamrollArray(arr) {
+// //   var str = arr.join('');
+// //   str = str.slice(4, 1);
+// //   console.log(str);
+// //   var newarr;
 
 
-  return newarr;
-}
-  steamrollArray([1, [2], [3, [[4]]]]);
+// //   return newarr;
+// }
+//   steamrollArray([1, [2], [3, [[4]]]]);
+
+
+describe("steamRollArray", function() {
+  it("Collapses nested arrays into a single array", function() {
+    expect(steamRollerArray([1, [2], [3, [[4]]]])).to.equal([1,2,3,4]);
+  });
+});
+
+var steamRollArray = function(array) {
+  var flattened = [];
+  array.forEach(function(element) {
+    if(Array.isArray(element)) {
+      flattened = flattened.concat(steamRollArray(element));
+      return flattened;
+    } else {
+      flattened.push(element);;
+    }
+  });
+  return array;
+};
+expect(steamRollArray([])).to.equal([]);
+expect(steamRollArray([3])).to.deep.equal([]);
+expect(steamRollArray(1,2,[3])).to.deep.equal(1,2,3);
+expect(steamRollArray([1])).to.deep.equal(1);
+
---
+++ InstagramClone/PracticeGallery3.js
@@ -0,0 +1,9 @@
+function showLightbox() {
+    document.getElementbyId("commentFormBG").style.display = "block";
+    document.getElementbyId("commentFormFG").style.display = "block";
+}
+
+function hideLightbox() {
+    document.getElementbyId("commentFormBG").style.display = "none";
+    document.getElementbyId("commentFormFG").style.display = "none";
+}
